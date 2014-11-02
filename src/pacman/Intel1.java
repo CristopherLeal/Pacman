11516b996b0dc529b4c6ffdd4d2726494812b623
Marcio <marciomacielbastos@hotmail.com>
2014-11-02 12:07:54 -0200

    Fantasma com inteligencia menos burra

--- src/pacman/Intel1.java
+++ src/pacman/Intel1.java
@@ -40,7 +40,7 @@ public class Intel1 implements Inteligencia {
             y=pos.get(iter).getY();
             iter++;
         }
-        if(iter==pos.size()){
+        if(iter==pos.size()||iter>4){
             iter=0;
             pos=logica.getPath(x , y);
         }
